2904e182dfcf0e3b97879d87eb11f3bc118a9822
lanjing <1530562965@qq.com>
2017-08-04 10:15:34 +0800

    进入项目开发

---
+++ src/components/NavBread.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="nav-breadcrumb-wrap">
+    <div class="container">
+      <nav class="nav-breadcrumb">
+        <a href="/">Home</a>
+        <slot name="bread"></slot>
+        <slot name="b"></slot>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
